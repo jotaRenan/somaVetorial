c8a4cfbbb4d778cc06c7178f8d45481dc81e905e
João Pedro Renan <jota.renan@gmail.com>
2016-07-14 20:08:08 -0300

    Suporta 2 vetores
    A fazer: botao de adicionar vetores e suporte a 3+ vetores de forma mais dinamica

--- prototipo.js
+++ prototipo.js
@@ -1,24 +1,58 @@
-function mudaCanvas(val) {
-  var seta = document.getElementById('seta');
+function rotacionar(val, vetores, index) {
+  var seta = vetores[index].getElementsByClassName('seta')[0];
   seta.style.transform = "rotate(" + (-val) + "deg)";
 }
 
+function resultante() {
+  var vetores = document.getElementsByClassName('vetor'),
+      arrVetores =  Array.prototype.slice.call(vetores),
+      arrModulos = [],
+      arrAngulos = [];
+  for (var i=0; i< vetores.length-1; i++) {
+    arrModulos.push(vetores[i].getElementsByClassName('modulo')[0].value);
+    arrAngulos.push(vetores[i].getElementsByClassName('valorFinal')[0].value);
+  }
+  
+  var angulo,
+      modResult,
+      a = arrModulos[0];
+      b = arrModulos[1];
+  angulo= Math.max(arrAngulos[0], arrAngulos[1]) - Math.min(arrAngulos[0], arrAngulos[1]);
+  angulo*=(Math.PI/180);
+  modResult = Math.sqrt( Math.pow(a,2) + Math.pow(b, 2) - 2*a*b*Math.cos(Math.PI-angulo) );
+  console.log(a + ' ' + ' ' + b + ' ' + angulo);
+  console.log(modResult);
+
+}
+
 window.onload = function() {
+  //--HABILITA EVENTOS DOS INPUTS
+  var vetores = document.getElementsByClassName('vetor'),
+      arrVetores =  Array.prototype.slice.call(vetores);
+  for (var i=0; i<vetores.length-1; i++) {
+    //---CASO ALTERE VALOR PELO RANGE
+    vetores[i].getElementsByClassName('valorRange')[0].addEventListener('input', function(e) {
+      e.target.parentNode.getElementsByClassName('valorNumber')[0].value = e.target.value;
+      e.target.parentNode.getElementsByClassName('valorFinal')[0].value = e.target.value;
 
-  document.getElementById('valorRange').addEventListener('input', function(e) {
-    document.getElementById('valorNumber').value = e.target.value;
-    document.getElementById('valorFinal').value = e.target.value;
-    mudaCanvas(e.target.value);
+      rotacionar(e.target.value, vetores, arrVetores.indexOf(e.target.parentNode));
+    }, false);
+
+    //---CASO ALTERE VALOR PELO INPUT
+    vetores[i].getElementsByClassName('valorNumber')[0].addEventListener('input', function(e) {
+      var valor = e.target.value;
+      while (valor > 360) {     //alguem melhora essa parada aqui pq ta um lixo
+        valor -= 360;
+      }
+      valor = (valor < 0) ? eval(360+valor) : valor;
+      e.target.parentNode.getElementsByClassName('valorRange')[0].value = valor;
+      e.target.parentNode.getElementsByClassName('valorFinal')[0].value = valor;
+      rotacionar(valor, vetores, arrVetores.indexOf(e.target.parentNode));
+    }, false);
+  }
+
+  //---EVENTO DE RESULTADO
+  document.getElementById('resultado').addEventListener('click', function() {
+    resultante();
   }, false);
-
-  document.getElementById('valorNumber').addEventListener('input', function(e) {
-    var valor = e.target.value;
-    while (valor > 360) {     //alguem melhora essa parada aqui pq ta um lixo
-      valor -= 360;
-    }
-    document.getElementById('valorRange').value = valor;
-    document.getElementById('valorFinal').value = valor;
-    mudaCanvas(valor);
-  }, false);
-
 };
