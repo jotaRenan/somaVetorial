1037ebcef4cbb99f381195b8ceb1c6bd1bd8499a
Joao Pedro Renan <jota.renan@gmail.com>
2016-08-15 17:28:15 -0300

    Subtracao multiplos unitarios
    Conserta bug que nao mostrava modulo das operacoes com unitarios e permite a subtracao destes. Tambem melhora pequenos trechos de codigos e padroniza documento HTML

--- scripts/vetorUnitario.js
+++ scripts/vetorUnitario.js
@@ -2,25 +2,40 @@
 
 function calcResultanteVetUnitario() {
   let vetUnitarioEls = document.getElementsByClassName('vetUn'),
-      vetUnRes = document.getElementsByClassName('vetUn-resultante')[0],
-      //--Valores dos componentes inseridos por usuario
+      vetUnResEl = document.querySelector('.vetUn-resultante'),
+      //--Valores dos componentes inseridos por usuario #j
       compI = [],
       compJ = [],
-      //--Componentes resultantes
+      //--Componentes resultantes #j
       resultanteComponenteI,
       resultanteComponenteJ;
 
   for ( let vetorUnitario of vetUnitarioEls) {
-    //---Vai colocando, nos arrays, os valores inseridos. 
-    compI.push(parseFloat(vetorUnitario.querySelector('.compI').value));
-    compJ.push(parseFloat(vetorUnitario.querySelector('.compJ').value));
+    //---Vai colocando, nos arrays, os valores inseridos. #j
+    let valorCompI = parseFloat(vetorUnitario.querySelector('.compI').value),
+        valorCompJ = parseFloat(vetorUnitario.querySelector('.compJ').value);
+    //---Verifica se usuario deseja subtrair ao inves de adicionar
+    if (vetorUnitario.querySelector('.cbx-subtracao-un').checked) {
+      valorCompI *= (-1);
+      valorCompJ *= (-1);
+    }
+    compI.push(valorCompI);
+    compJ.push(valorCompJ);
   }
-  //---Soma os valores e exibe resultado.
-  resultanteComponenteI = vetUnRes.querySelector('.resI');
-  resultanteComponenteI.value = calcTotal(compI);
+  //---Soma os valores das componentes #j
+  let componenteIResultado = calcTotal(compI),
+      componenteJResultado = calcTotal(compJ),
+      moduloVetorResultante;
 
-  resultanteComponenteJ = vetUnRes.querySelector('.resJ');
-  resultanteComponenteJ.value = calcTotal(compJ);
+  //---Exibe valores das componentes #j
+  resultanteComponenteI = vetUnResEl.querySelector('#resI');
+  resultanteComponenteI.value = componenteIResultado;
+  resultanteComponenteJ = vetUnResEl.querySelector('#resJ');
+  resultanteComponenteJ.value = componenteJResultado;
+  //---Calcula e exibe modulo do vetor resultante #j
+  moduloVetorResultante = Math.sqrt( Math.pow(componenteJResultado, 2) + 
+    Math.pow(componenteIResultado, 2) );
+  vetUnResEl.querySelector('.modulo').value = round(moduloVetorResultante, 3);
 }
 
 function calcTotal(comp) {
@@ -32,18 +47,31 @@ function calcTotal(comp) {
 }
 
 function acrescentarVetUn() {
-  //--Cria elementos DOM
-  let section = cria('section'),
-      titulo = cria('h3'),
-      label = cria('label'),
-      inputs = cria('input');
+  /*  Essa funcao permite a geracao dinamica de campos para o usuario inserirmais
+   *  vetores unitarios. Cria-se cada elemento no DOM e cada um eh concatenado
+   *  ao documento. #j
+  */
+  let label, inputs,
+      section = cria('section'),
+      titulo = cria('h3');
+
   const numeroDoProximoVetor = (document.getElementsByClassName('vetUn').length + 1);
 
   section.classList.add('vetUn');
-  titulo.innerHTML = `Vetor ${numeroDoProximoVetor}`;
+  titulo.textContent = `Vetor ${numeroDoProximoVetor}`;
   section.appendChild(titulo);
 
-  label.innerHTML = 'Valor de î:';
+  label = cria('label');
+  label.textContent = 'Subtração';
+  inputs = cria('input');
+  inputs.type = 'checkbox';
+  inputs.classList.add('cbx-subtracao-un');
+  label.appendChild(inputs);
+  section.appendChild(label);
+
+  label = cria('label');
+  inputs = cria('input');
+  label.textContent = 'Valor de î:';
   inputs.value = 0;
   inputs.type = 'number';
   inputs.classList.add('compI');
@@ -51,7 +79,7 @@ function acrescentarVetUn() {
   section.appendChild(label);
 
   label = cria('label');
-  label.innerHTML = 'Valor de j:';
+  label.textContent = 'Valor de j:';
   inputs = cria('input');
   inputs.value = 0;
   inputs.type = 'number';
@@ -60,7 +88,7 @@ function acrescentarVetUn() {
   section.appendChild(label);
 
   label = cria('label');
-  label.innerHTML = 'Valor de u:';
+  label.textContent = 'Valor de u:';
   inputs = cria('input');
   inputs.disabled = 'true';
   label.appendChild(inputs);
@@ -68,23 +96,14 @@ function acrescentarVetUn() {
 
   const vetUnEls = document.getElementsByClassName('vetUn');
   insertAfter(section, vetUnEls[vetUnEls.length-1]);
-
-  const nmrVetoresUnitarios = document.getElementsByClassName('vetUn').length;
-  document.getElementById('addUn').disabled = nmrVetoresUnitarios === 5;
-  document.getElementById('remUn').disabled = nmrVetoresUnitarios <= 2; 
-
-
+  controlaAdicaoRemocaoVetUn();
+  
 }
 
 function removerVetUn() {
-
   let vetUnEl = document.getElementById('vetores-unitarios');
   vetUnEl.removeChild(vetUnEl.querySelector('.vetUn:last-child'));
-
-  let nmrVet = document.getElementsByClassName('vetUn').length;
-  document.getElementById('remUn').disabled = nmrVet === 2;
-  document.getElementById('addUn').disabled = nmrVet < 2;
-
+  controlaAdicaoRemocaoVetUn();
 }
 
 function cria(el) {
@@ -93,4 +112,13 @@ function cria(el) {
 
 function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+/*  Definido um limite de vetores, controla os botoes de Adicionar Vetor e
+      Remover Vetor. #j
+  */
+function controlaAdicaoRemocaoVetUn() {
+  const nmrVet = document.getElementsByClassName('vetUn').length;
+  document.getElementById('addUn').disabled = nmrVet === 2;
+  document.getElementById('remUn').disabled = nmrVet === 5;
 }
