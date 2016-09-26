e1398d116d92362bde28b05e4d9cbcf3e8936f53
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-09-26 11:49:30 -0300

    criar métodos conversores unitário <-> padrão

--- scripts/classeVetorPadrao.js
+++ scripts/classeVetorPadrao.js
@@ -16,8 +16,56 @@ class vetorPadrao {
     }
   }
 
+  convertePadraoUnit(values) {
+    //values é um vetor que vai conter em 0 o módulo e em 1 o ângulo do vetor
+    let modulo,
+        radiano,
+        sinAng,
+        angulo,
+        compI,
+        compJ,
+        compK;
+    modulo = values[0];
+    angulo = values[1];
+    //transforma angulo em rad
+    radiano = (Math.PI * angulo)/180;
+    //calcula os componentes
+    compK = 0; //componente K não será necessário
+    compJ = Math.sin(radiano) * modulo;
+    sinAng = Math.sin(radiano);
+    compI = Math.cos(radiano) * modulo;
+    //a função retornará um vetor na ordem I, J, K
+    return [compI, compJ, compK];
+  }
+
+  converteUnitPadrao(values) {
+    //values é um vetor que vai conter em 0 o compI, em 1 o compJ e em 2 o compK
+    let modulo,
+        angulo,
+        radiano,
+        compI,
+        compJ,
+        compK,
+        senAng;
+    compI = values[0];
+    compJ = values[1];
+    compK = values[2];
+    //calcula o modulo a partir do componente
+    modulo = parseFloat(Math.sqrt( Math.pow(compI, 2) + Math.pow(compJ, 2) + Math.pow(compK, 2) ));
+    //calcula o angulo
+    senAng = (compI/modulo);
+    radiano = Math.asin(senAng);
+    angulo = (radiano*180)/Math.PI;
+    //a função retornará um vetor na ordem módulo, ângulo
+    return [modulo, angulo];
+  }
+
   calculaResultante() {
-    //-- Hey, Paula: do the math!
+    let compI,
+        compJ,
+        compK;
+
+
   }
 
   set modulo(mod) {
